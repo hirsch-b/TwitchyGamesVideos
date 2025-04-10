fa569757b3752d45f626210b107859b1c5781cf0
hirsch-b <hirsch-b@users.noreply.github.com>
2025-04-10 18:33:11 +0200

    Fixed wrong variable in MongoDB configuration

--- twitchybackend/db.py
+++ twitchybackend/db.py
@@ -7,7 +7,7 @@ def init_mongodb():
     host = environ.get("MONGODB_HOST", "mongo")
     host = environ.get("MONGODB_HOST", "localhost")
     port = environ.get("MONGODB_PORT", "27017")
-    db = environ.get("MONGODB_PORT", "twitchybackend")
+    db = environ.get("MONGODB_DB", "twitchybackend")
     mongodb_connection = connect(
         host=f"mongodb://{host}:{port}/{db}",
         uuidRepresentation="standard",
