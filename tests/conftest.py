f4be2b92303149c23a7f75f576895191b24b5eb9
hirsch-b <hirsch-b@users.noreply.github.com>
2025-05-06 06:27:05 +0200

    Streamline testing with a database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
+from os import environ
 from fastapi.testclient import TestClient
 from pytest import fixture
-
-from twitchybackend.app import app
 
 
 @fixture
 def httpclient():
+    from twitchybackend.app import app, mongodb
+    db_name = mongodb.get_database().name
+    assert db_name == environ["MONGODB_DB"]
+    mongodb.drop_database(db_name)
     yield TestClient(app)
+    mongodb.drop_database(db_name)
+
+environ["PYTESTING"] = "true"
+environ["MONGODB_DB"] = "unittesting"
