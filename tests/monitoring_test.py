835003b1c6e00ab74557ca128d35e63677185554
hirsch-b <hirsch-b@users.noreply.github.com>
2025-05-06 06:27:38 +0200

    Added status endpoint test, but not started

--- tests/monitoring_test.py
+++ tests/monitoring_test.py
@@ -2,3 +2,8 @@ def test_heartbeat(httpclient):
     response = httpclient.get("/monitoring/heartbeat")
     assert response.status_code == 200
     assert "now" in response.json()
+
+
+def test_status(httpclient):
+    response = httpclient.get("/monitoring/status")
+    assert response.status_code == 200
